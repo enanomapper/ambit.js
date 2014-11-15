2d24a86f713c0fa3645e9a2dd7607c285b6df6e4
Egon Willighagen <egon.willighagen@gmail.com>
2014-11-15 07:55:22 +0100

    Added a method to get the info for a substance

--- src/Substance.js
+++ src/Substance.js
@@ -9,7 +9,19 @@ Ambit.Substance.prototype.list = function(callback) {
 		url: this.baseURL + "/substance",
                 dataType: 'json',
 		success: function(response, status, request) {
-			console.log(response);
+			callback.call(this, true, request.status, response);
+		},
+		error: function(request, status, error) {
+			callback.call(this, false, request.status);
+		}
+	});
+}
+
+Ambit.Substance.prototype.info = function(uri, callback) {
+	var conceptWikiSearcher = $.ajax({
+		url: uri + "/study",
+                dataType: 'json',
+		success: function(response, status, request) {
 			callback.call(this, true, request.status, response);
 		},
 		error: function(request, status, error) {
