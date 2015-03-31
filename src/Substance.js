242d241f4270738cd900fabc3e78fe86f61b69a2
Egon Willighagen <egon.willighagen@gmail.com>
2015-03-31 11:33:08 +0200

    Local copy of ambit.js v0.0.1

---
+++ src/Substance.js
@@ -0,0 +1,31 @@
+//This content is released under the MIT License, http://opensource.org/licenses/MIT. See licence.txt for more details.
+
+Ambit.Substance = function(baseURL) {
+	this.baseURL = baseURL;
+}
+
+Ambit.Substance.prototype.list = function(callback) {
+	var conceptWikiSearcher = $.ajax({
+		url: this.baseURL + "/substance",
+                dataType: 'json',
+		success: function(response, status, request) {
+			callback.call(this, true, request.status, response);
+		},
+		error: function(request, status, error) {
+			callback.call(this, false, request.status);
+		}
+	});
+}
+
+Ambit.Substance.prototype.info = function(uri, callback) {
+	var conceptWikiSearcher = $.ajax({
+		url: uri + "/study",
+                dataType: 'json',
+		success: function(response, status, request) {
+			callback.call(this, true, request.status, response);
+		},
+		error: function(request, status, error) {
+			callback.call(this, false, request.status);
+		}
+	});
+}
